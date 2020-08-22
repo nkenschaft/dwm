8ce24380ded440430fbb8c27dfe60effabecd1a9
Nathaniel Kenschaft <nkenschaft@gmail.com>
2020-08-21 23:58:38 -0400

    config

--- config.def.h
+++ config.def.h
@@ -11,8 +11,8 @@ static const unsigned int gappov    = 5;       /* vert outer gap between windows
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 10;       /* vertical padding of bar */
-static const int sidepad            = 10;       /* horizontal padding of bar */
+static const int vertpad            = 5;       /* vertical padding of bar */
+static const int sidepad            = 5;       /* horizontal padding of bar */
 static const char *fonts[]          = { "Hack:size=12", "MesloLGS:size=12" };
 static const char dmenufont[]       = "Hack:size=12";
 static const char col_gray1[]       = "#282a36"; /* "#222222"; /* normal background */
