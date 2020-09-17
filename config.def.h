28ddb118c22a8f0f6e62c9011a6f122f40e70c36
Nathaniel Kenschaft <nkenschaft@gmail.com>
2020-09-16 23:09:22 -0400

    updated dmemucmd

--- config.def.h
+++ config.def.h
@@ -69,7 +69,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-h", "29", "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]    = { "alacritty", NULL };
 static const char *volup5[]     = { "pulsemixer", "--change-volume", "+5" };
 static const char *voldown5[]   = { "pulsemixer", "--change-volume", "-5" };
